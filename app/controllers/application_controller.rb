3c2e917ac940027a1e1ad0ee093db02d94ecc499
star__hoshi <kensuke1751@gmail.com>
2016-05-17 00:16:00 +0900

    filter を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+  def authenticate
+    authenticate_token || render_unauthorized
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      User.find_by(token: token)
+    end
+  end
+
+  def render_unauthorized
+    self.headers['Authorization'] = 'Bearer realm="Application"'
+    render json: {:message => "Unauthorized"}, status: 401
+  end
 end
