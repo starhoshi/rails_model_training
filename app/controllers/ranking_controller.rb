4ab7ca45d452a2704f5fada2a282e9e1cf29c933
star__hoshi <kensuke1751@gmail.com>
2016-05-22 01:50:47 +0900

    ranking/songs の追加

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,11 @@
 class RankingController < ApplicationController
   before_action :authenticate
 
-  def index
+  def songs
+    @songs = SongTotalCount.joins(:user, :song).select("song_total_counts.*, songs.*").where(user_id: @user.id).order(play_count: :desc)
+  end
+
+  def artist
     @songs = SongTotalCount.joins(:user, :song).select("song_total_counts.*, songs.*").where(user_id: @user.id).order(play_count: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'ranking',to: 'ranking#index',  defaults: { format: :json }
+  get 'ranking/songs',to: 'ranking#songs',  defaults: { format: :json }
 
   resources :push_tokens, defaults: { format: :json }
   resources :songs, defaults: { format: :json }
