cfaf9d2e31b30ec81aa67a200e2c04a8ac4b1a71
star__hoshi <kensuke1751@gmail.com>
2016-05-21 21:23:16 +0900

    users に privacy 追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520141830) do
+ActiveRecord::Schema.define(version: 20160521122045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20160520141830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "auth_token"
+    t.string   "privacy"
   end
 
   add_foreign_key "devices", "users"
