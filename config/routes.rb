b21aae2b27e239f0b5fc28e7b983f5db0841c25b
star__hoshi <kensuke1751@gmail.com>
2016-05-21 17:25:26 +0900

    defaults: { format: :json }

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   #resources :users
 
   resources :users, only: [:index, :create, :new]
-  resource :user, path: '/users/me', only: [:show, :edit, :update, :destroy]
+  resource :user, path: '/users/me', only: [:show, :edit, :update, :destroy], defaults: { format: :json }
 
-  resources :song_histories
+  resources :song_histories, only: [:index, :create, :new, :edit, :update, :destroy], defaults: { format: :json }
+  resource :user, path: '/song_histories/summary', only: [:show], defaults: { format: :json }
   get 'auth/:provider/callback', to: 'callback#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
