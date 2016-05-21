7b492d941bbc83322130d179b61a6d21f8e6800e
star__hoshi <kensuke1751@gmail.com>
2016-05-21 17:42:09 +0900

    change resource name.

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :create, :new]
   resource :user, path: '/users/me', only: [:show, :edit, :update, :destroy], defaults: { format: :json }
 
-  resources :song_histories, only: [:index, :create, :new, :edit, :update, :destroy], defaults: { format: :json }
-  resource :user, path: '/song_histories/summary', only: [:show], defaults: { format: :json }
+  resource :song_histories, only: [:show, :create, :new, :edit, :update, :destroy], defaults: { format: :json }
+  get 'song_histories/summary', to: 'song_histories#index', defaults: { format: :json }
   get 'auth/:provider/callback', to: 'callback#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
