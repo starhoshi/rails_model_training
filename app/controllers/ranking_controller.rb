ea1875cc9557ac6a642ae28ce8779e133f13c09d
star__hoshi <kensuke1751@gmail.com>
2016-05-22 13:34:08 +0900

    パラメータをへんこう

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,5 +1,5 @@
 class RankingController < ApplicationController
-  before_action :authenticate
+  before_action :authenticate, only: [:my_songs, :my_artists]
 
   def my_songs
     @songs = SongTotalCount.joins(:user, :song).select("song_total_counts.*, songs.*").where(user_id: @user.id).order(play_count: :desc)
@@ -9,6 +9,17 @@ class RankingController < ApplicationController
     @artists = SongTotalCount.joins(:user, :song)
       .select('songs.artist, SUM(song_total_counts.play_count) AS play_count, SUM(song_total_counts.skip_count) AS skip_count')
       .where(user_id: @user.id)
+      .group('songs.artist')
+      .order('play_count DESC')
+  end
+
+  def songs
+    @songs = SongTotalCount.joins(:song).select("song_total_counts.*, songs.*").order(play_count: :desc)
+  end
+
+  def artists
+    @artists = SongTotalCount.joins(:song)
+      .select('songs.artist, SUM(song_total_counts.play_count) AS play_count, SUM(song_total_counts.skip_count) AS skip_count')
       .group('songs.artist')
       .order('play_count DESC')
   end
